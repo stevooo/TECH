fd3beb5285ce49852d324bea9a579131e54990ba
Stephen McFarlane <stm26@aber.ac.uk>
2015-07-25 14:28:12 +0100

    Update: hide sensitive database details

--- TECH/phpscript/dbconnection.php
+++ TECH/phpscript/dbconnection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = pg_connect("host=db.dcs.aber.ac.uk dbname=cs399_14_15_stm26 user=stm26 password=stephen19");
+$conn = pg_connect("host=************** dbname=********** user=***** password=*********");
 ?>
